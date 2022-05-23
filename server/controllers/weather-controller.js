bc9481660ef331900c5c002ef09516415c4d3e99
@spartanpizza <lourencowlov@gmail.com>
2022-05-23 13:51:29 +0200

    error handling and validation

--- server/controllers/weather-controller.js
+++ server/controllers/weather-controller.js
@@ -3,81 +3,105 @@ const request = require("request")
 
 
 //gets Weather from OpenWeatherMap and pass data to callback
+
+
 exports.getWeather= async (req,res,next)=>{
 
-    var params = req.query
+        var params = req.query
 
-    console.log(params)
+        console.log(params)
 
-    if(params["lat"] && params["lon"]){
-        getWeatherFromCoordinates(params,(err,data)=>{
-            if (err!=null){
-                //throw error
-               return res.json({err},500)
-            }
+        const exceptionCord = ({message, code}) => {
+            res.status(code).json({message})
+        }
+        const exceptionZip=({message, code})=>{
+            res.status(code).json({message})
+        }
+
+
+    
+        if(params["lat"] && params["lon"]){
+            getWeatherFromCoordinates(params).then(data => {
+                res.json(JSON.parse(data.body))
+            })
+            .catch(exceptionCord)
+        }
+        else if(params["zip"]){
+            getWeatherFromZipCode(params).then(data=>{
+                res.json(JSON.parse(data.body))
+            })
+            .catch(exceptionZip)
+        }
+        else {
+            // res.sendStatus(418)
+           res.status(400).json('Expected param zip | [lon, lat]')
             
-            res.send(JSON.parse(data.body))
-        })
-    }
-    else if(params["zip"]){
-        getWeatherFromZipCode(params,(err,data)=>{
-            if (err!=null){
-                //throw error
-            }
-            res.send(JSON.parse(data.body))
-        })
-    }
-    else {
-        // res.sendStatus(418)
-        res.json({err: 'Expected param zip | [lon, lat]'},400)
-        return
-    }
-
-
+        }
+        
+        
 
 
 }
 
 //If countrycode not specified default is SouthAfrica
-function getWeatherFromZipCode(params,nextFunction){
+function getWeatherFromZipCode(params){
+    return new Promise((yay,nay)=>{
+        var zipcode = params["zip"]
+        if(isNaN(zipcode) ){return nay({message: 'zip must be a number',code:400})}
+       // if(zipcode.length !==4){return nay({message: 'zip must be 4 digits long, sorry',code:400})}
 
-    var zipcode = params["zip"]
-    var countrycode = params["countrycode"]||"ZA"
-    console.log(countrycode)
-    var unit = (params["unit"]||"metric")
-    let options = {
-        url: "https://api.openweathermap.org/data/2.5/weather",
-        method: 'GET',
-        qs: {
-            zip: zipcode+","+countrycode,
-            units:unit,
-            appid:'7b25fd89096db406853de3ba537ecd99',
+        var countrycode = params["countrycode"]||"ZA"
+        if(!isNaN(countrycode)){return nay({message: 'country code must be text', code:400})}
+        if(countrycode.length !==2){return nay({message: 'country code must be 2 digits', code:400})}
+
+
+        console.log(countrycode)
+        var unit = (params["unit"]||"metric")
+        let options = {
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            method: 'GET',
+            qs: {
+                zip: zipcode+","+countrycode,
+                units:unit,
+                appid:'7b25fd89096db406853de3ba537ecd99',
+            }
         }
-    }
-
-    request(options,(err,data)=>{
-        nextFunction(err,data)
+        console.log(options)
+        request(options,(err,data)=>{
+            if(err){
+                return nay({message: err, code:data.cod})
+            }
+            return yay(data)
     })
+    })
+    
 }
 
-function getWeatherFromCoordinates(params,next){
-
-
-    var lat = params["lat"]
-    var lon = params["lon"]
-    if(isNaN(lat) || isNaN(lon) ){ return next('lat | lon must be number')}
-    let options = {
-        url: "https://api.openweathermap.org/data/2.5/weather",
-        method: 'GET',
-        qs: {
-            lat: lat,
-            lon:lon,
-            appid:'7b25fd89096db406853de3ba537ecd99',
+function getWeatherFromCoordinates(params){
+    return new Promise((yay, nay) => {
+        var lat = params["lat"]
+        var lon = params["lon"]
+        if(isNaN(lat) || isNaN(lon) ){ 
+            return nay({message: 'lat | lon must be number', code: 400})
         }
-    }
 
-    request(options,next)
-
+        let options = {
+            url: "https://api.openweathermap.org/data/2.5/weather",
+            method: 'GET',
+            qs: {
+                lat: lat,
+                lon:lon,
+                appid:'7b25fd89096db406853de3ba537ecd99',
+            }
+        }
+        console.log(options)
+        request(options,(err, data) => {
+            if(err) {
+                return nay({message: err, code: data.cod})
+            }
+            yay(data)
+        })
+    })
 }
 
 //gets WeatherObj and pass to callback statement
@@ -92,7 +116,7 @@ exports.getWeatherInfoFromCode=async (req,res)=>{
 
     weather.getWeatherFromCode(code,isDayTime,(err,data)=>
     {
-        if (err!==null){
+        if (err){
             res.json({err})
             return
         }
